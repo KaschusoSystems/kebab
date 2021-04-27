988cc3bc731e9c006e53d8a83997429851e7b77a
RomanL1 <lysserroman@gmail.com>
2021-04-27 14:30:45 +0200

    fixed grades endpoint

--- routes/api/grades.js
+++ routes/api/grades.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose');
 var router = require('express').Router();
 var Grade = mongoose.model('Grade');
+var User = mongoose.model('User');
+var Subject = mongoose.model('Subject');
 var auth = require('../auth');
 
 router.get('/', auth.required, async function (req, res, next) {
   const userId = req.payload.id;
-  const grades = await Grade.find({'user': userId});
-  return res.json(grades.map(x => x.toJSONFor()));
+  const subjects = await Subject.find({'user': userId});
+  return res.json(subjects.map(x => x.toJSONFor()));
 });
 
 module.exports = router;
