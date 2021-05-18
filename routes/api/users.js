a2ab067985c586129b173a7429446d6e6a294612
Florin Barbisch <florin.barbisch@gmail.com>
2021-05-18 19:02:43 +0200

    Added: missing endpoint for gyros

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,15 @@ var passport = require('passport');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
+router.get('/user', auth.required, function (req, res, next) {
+  User.findById(req.payload.id).then(function (user) {
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    return res.json({ user: user.toProfileJSONFor() });
+  }).catch(next);
+});
+
 router.put('/user', auth.required, function (req, res, next) {
   User.findById(req.payload.id).then(function (user) {
     if (!user) {
